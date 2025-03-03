8e27d63381122987c44f43a5db038bce921c01d7
= <=>
2025-03-04 01:26:06 +0530

    update database settings

--- book_recommender/settings.py
+++ book_recommender/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'book_recommender.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookrecommendation',
+        'USER' : 'root',
+        'PASSWORD' : 'Shahbaz@1505',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
     }
 }
 
