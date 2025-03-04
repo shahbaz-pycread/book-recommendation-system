80577d84a29acba7fae85e93455514a207893e18
= <=>
2025-03-04 20:17:03 +0530

    updated book_detail view - views to handle form submission and display the form for submitting a rating

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book, UserRating
 from .recommendations import recommend_books
-
+from .forms import RatingForm
 # Create your views here.
 
 def books_list(request):
@@ -13,11 +13,29 @@ def books_list(request):
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    
+
     # Get recommendation for this book
     recommended_books = recommend_books(book.title)
+    
+    # Handling Rating form
+    if request.method == 'POST':
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            rating = form.save(commit=False)
+            rating.book = book
+            rating.user = request.user # Associate the rating with the current user
+            rating.save()
+            return redirect('book_detail', book_id = book_id) # Refresh the page after form submission
+    else:
+        form = RatingForm()
+        
+    # Get all existing ratings for this book
+    ratings = UserRating.objects.filter(book=book)
     context = {
         'book' : book,
-        'recommended_books' : recommended_books
+        'recommended_books' : recommended_books,
+        'form' : form,
+        'ratings' : ratings
     }
     return render(request, 'books/book_detail.html', context)
+
