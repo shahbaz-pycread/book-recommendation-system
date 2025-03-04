09166cb3f2976296e2903d30ae9d52b4bebde641
= <=>
2025-03-04 13:24:05 +0530

    configure settings to handle media files

--- book_recommender/settings.py
+++ book_recommender/settings.py
@@ -126,6 +126,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'static_cdn')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
