08f76fddfec8f30e16e9fdb40e0b69c2d07d3b4e
= <=>
2025-03-05 07:21:17 +0530

    update the urls - add the path for 'Add Book' feature

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('books/', views.books_list, name='all_books'),
     # Define the URL for book detail view
     path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('add-book/', views.add_book, name='add-book'),
     path('signup/', views.signup_view, name='signup'),
     path('login/',auth_views.LoginView.as_view(template_name='books/registration/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout')
