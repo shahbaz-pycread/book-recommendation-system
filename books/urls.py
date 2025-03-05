b883e0884c63056eb014c66be5655421503e057b
= <=>
2025-03-05 05:48:59 +0530

    update urls.py - includes paths for the sign-up, login, and logout views

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views  # Import your views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('books/', views.books_list, name='all_books'),
     # Define the URL for book detail view
     path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/',auth_views.LoginView.as_view(template_name='books/registration/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout')
 ]
